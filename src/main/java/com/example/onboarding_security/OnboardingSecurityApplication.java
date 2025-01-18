ff099bad015b860b44fa3eeb0a99311267fc58a0
shinheekim <dev.shinheekim@gmail.com>
2025-01-18 13:59:57 +0900

    docs: README.md 생성

---
+++ src/main/java/com/example/onboarding_security/OnboardingSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.onboarding_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnboardingSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onboarding_security/OnboardingSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onboarding_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
