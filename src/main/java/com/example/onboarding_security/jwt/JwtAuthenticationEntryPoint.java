956c0505d52d10d2637eff831f91885b33f04ef1
shinheekim <dev.shinheekim@gmail.com>
2025-01-19 13:04:11 +0900

    chore(entrypoint): json형태로 401 error 응답

---
+++ src/main/java/com/example/onboarding_security/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,35 @@
+package com.example.onboarding_security.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authenticationException) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        Map<String, Object> res = new HashMap<>();
+        res.put("statusCode", HttpServletResponse.SC_UNAUTHORIZED);
+        res.put("message", authenticationException.getMessage());
+
+        OutputStream out = response.getOutputStream();
+        objectMapper.writeValue(out, res);
+        out.flush();
+    }
+}
