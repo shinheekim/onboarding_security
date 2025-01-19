0e2e267edb8f9cb77d4b1986733771b17154b619
shinheekim <dev.shinheekim@gmail.com>
2025-01-19 11:28:52 +0900

    feat(filter): JwtAuthenticationFilter 생성 및 SecurityConfig에서 FilterChain에 추가

---
+++ src/main/java/com/example/onboarding_security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,50 @@
+package com.example.onboarding_security.jwt;
+
+import com.example.onboarding_security.exception.CustomAuthenticationException;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String BEARER_PREFIX = "Bearer ";
+    private final JwtUtil jwtUtil;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            String token = resolveToken(request);
+            if (StringUtils.hasText(token) && jwtUtil.validateToken(token)) {
+                Authentication authentication = jwtUtil.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                log.info("Security Context에 인증 정보를 저장했습니다. 인증된 사용자: {}", authentication.getPrincipal());
+            }
+            filterChain.doFilter(request, response);
+        } catch (CustomAuthenticationException e) {
+            log.error("JWT 인증 실패: {}", e.getMessage());
+            jwtAuthenticationEntryPoint.commence(request, response, new CustomAuthenticationException(e.getMessage()));
+        }
+    }
+    private String resolveToken(HttpServletRequest request) {
+        String token = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(token) && token.startsWith(BEARER_PREFIX)) {
+            return token.substring(BEARER_PREFIX.length());
+        }
+        return null;
+    }
+}
